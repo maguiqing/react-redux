421a6ba32eb2d9e8e6d22a3c9b9be8697bcc145f
maguiqing <maguiqing@xiaomi.com>
2020-12-14 18:25:03 +0800

    feat: update

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Counter from './Counter';
-// import { Provider } from 'react-redux';
-import Provider from './Provider';
+import { Provider } from 'react-redux';
+// import Provider from './Provider';
 import store from './models/index';
 import reportWebVitals from './reportWebVitals';
 
